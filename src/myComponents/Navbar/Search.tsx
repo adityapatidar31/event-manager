cc3e4f21a6272d75fea64b890207f077ec41b7cb
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-11 15:31:33 +0530

    add debounce in the code

--- src/myComponents/Navbar/Search.tsx
+++ src/myComponents/Navbar/Search.tsx
@@ -1,19 +1,19 @@
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
-// import { useDebouncedCallback } from "use-debounce";
-// import { useNavigate } from "react-router-dom";
+import { useDebouncedCallback } from "use-debounce";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchInput() {
   const [search, setSearch] = useState("");
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const debounced = useDebouncedCallback((query) => {
-  //   if (query.length < 3) {
-  //     navigate("?", { replace: true });
-  //   } else {
-  //     navigate(`?search=${query}`, { replace: true });
-  //   }
-  // }, 1000);
+  const debounced = useDebouncedCallback((query) => {
+    if (query.length < 3) {
+      navigate("?", { replace: true });
+    } else {
+      navigate(`?search=${query}`, { replace: true });
+    }
+  }, 1000);
 
   return (
     <Input
@@ -21,9 +21,9 @@ export default function SearchInput() {
       value={search}
       onChange={(e) => {
         setSearch(e.target.value);
-        // debounced(e.target.value);
+        debounced(e.target.value);
       }}
-      placeholder="Search Tour"
+      placeholder="Search Event"
     />
   );
 }

affad5f4e43b68dbc09238ab206436a512ecb199
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-12 06:08:35 +0530

    change the logic of socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,5 +2,7 @@ import { io } from "socket.io-client";
 import { BASE_URL } from "./backend";
 
 export const socket = io(BASE_URL, {
+  transports: ["websocket", "polling"], // Ensures WebSocket fallback
+  withCredentials: true, // Needed if cookies/sessions are used
   autoConnect: false,
 });
